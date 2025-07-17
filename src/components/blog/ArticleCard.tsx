64ab68583d88691c016ea8cab32dde58224ffbce
Isaias Castillo <isaias.castillo@utp.ac.pa>
2025-07-17 13:10:32 -0500

    feat: Update BlogPage to include ArticleCard component with structured layout and improved styling

--- src/components/blog/ArticleCard.tsx
+++ src/components/blog/ArticleCard.tsx
@@ -1,39 +1,43 @@
-import React from 'react'
+import React from "react";
 type Props = {
-    imgSrc?: string;
-    title?: string;
-    description?: string;
-    authorImgSrc?: string;
-    authorName?: string;
-    date?: string;
-}
+  colRow?: string;
+  imgSrc?: string;
+  title?: string;
+  description?: string;
+  authorImgSrc?: string;
+  authorName?: string;
+  date?: string;
+};
 export default function ArticleCard({
-    imgSrc = '',
-    title = '',
-    description = '',
-    authorImgSrc = '',
-    authorName = '',
-    date = ''
-    }: Props) {
+  colRow = "row",
+  imgSrc = "",
+  title = "",
+  description = "",
+  authorImgSrc = "",
+  authorName = "",
+  date = "",
+}: Props) {
   return (
     <>
-        <article>
-            {imgSrc && <img src={imgSrc} alt={title} />}
+      <article className={`flex flex-${colRow} gap-4 p-4`}>
+        {imgSrc && <img src={imgSrc}/>}
+        <div>
+          <div>
+            <h2 className="text-2xl font-bold md:text-3xl lg:text-5xl">{title}</h2>
+            <img src="" alt="" />
+          </div>
+          <p className="text-base md:text-lg lg:text-xl text-secundario">{description}</p>
+          <div>
+            {authorImgSrc && <img src={authorImgSrc} alt={authorName} />}
             <div>
-                <div>
-                <h2>{title}</h2>
-                <img src="" alt="" />
-                </div>
-                <p>{description}</p>
-                <div>
-                    {authorImgSrc && <img src={authorImgSrc} alt={authorName} />}
-                    <div>
-                        <h5>{authorName}</h5>
-                        <time>{date}</time>
-                    </div>
-                </div>
+              <h5 className="mb-4 text-lg md:text-xl lg:text-2xl font-semibol">{authorName}</h5>
+              <time>{date}</time>
             </div>
-        </article>
+          </div>
+        </div>
+      </article>
+         <hr className="w-full my-4 border-gray-300 border-t-1" />
+
     </>
-  )
+  );
 }
